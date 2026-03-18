74dfdff69aaba4c45d06ee7ba8b6dd80609561a3
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-03-18 22:27:28 +0000

    feat: auth middleware

---
+++ maintenance-tracker-backend/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (req, res, next) => {
+  const authHeader = req.header("Authorization");
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token, authorization denied" });
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token is not valid" });
+  }
+};
+
+const adminMiddleware = (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return res.status(403).json({ message: "Access denied. Admin only." });
+  }
+  next();
+};
+
+module.exports = { authMiddleware, adminMiddleware };
