9868a9b2ed7074274454b624e9c97037455eba0a
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-03-20 01:54:42 +0000

    style: border color changed, bg-color removed

--- rentflow-maintenance-frontend/src/components/Input.jsx
+++ rentflow-maintenance-frontend/src/components/Input.jsx
@@ -32,7 +32,7 @@ const Input = ({
           w-full py-2 px-3 border rounded-md 
           focus:outline-none focus:border-transparent
           focus:ring-2 focus:ring-blue-600 
-          border-none bg-gray-200 transition duration-300
+          border-gray-300  transition duration-300
           ${inputClassName}
           `}
       />
