ffd050a7eee2ae23273f88f2d4b3fded77e5dff9
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-03-23 00:40:16 +0000

    feat: added Home

---
+++ rentflow-maintenance-frontend/src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import DashboardCard from "../components/DashboardCard";
+import { Wrench, CircleAlert, Clock4, CircleCheckBig } from "lucide-react";
+
+const Home = () => {
+  const [value, setValue] = useState({
+    totalRequests: 10,
+    pending: 0,
+    inProgress: 0,
+    resolved: 0,
+  });
+
+  // Card Data
+  const mockCardData = [
+    {
+      title: "Total Requests",
+      icon: Wrench,
+      value: value.totalRequests,
+      color: "blue",
+    },
+    {
+      title: "Pending",
+      icon: CircleAlert,
+      value: value.pending,
+      color: "gray",
+    },
+    {
+      title: "In Progress",
+      icon: Clock4,
+      value: value.inProgress,
+      color: "yellow",
+    },
+    {
+      title: "Resolved",
+      icon: CircleCheckBig,
+      value: value.resolved,
+      color: "green",
+    },
+  ];
+  return (
+    <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mb-8">
+        {mockCardData.map((card, index) => (
+          <DashboardCard
+            key={index}
+            title={card.title}
+            icon={card.icon}
+            value={card.value}
+            color={card.color}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
