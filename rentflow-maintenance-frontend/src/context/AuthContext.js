ee520a18c74abd4f9eae8a4c25beb835f1da03c1
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-04-06 01:05:06 +0000

    refactor: imported url from .env

--- rentflow-maintenance-frontend/src/context/AuthContext.js
+++ rentflow-maintenance-frontend/src/context/AuthContext.js
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   const axiosWithAuth = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: process.env.REACT_APP_API_URL,
   });
 
   axiosWithAuth.interceptors.request.use(
