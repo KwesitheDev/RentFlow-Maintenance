30abc3dd890312f123739e2c95cd7a3347e01240
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2026-04-07 10:32:07 +0000

    fix: corrected typo in vercel url

--- maintenance-tracker-backend/server.js
+++ maintenance-tracker-backend/server.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000", //local development
-      "https://rent-flow-maintenance.vercel.app", //vercel deployement
+      "https://rentflow-maintenance.vercel.app", //vercel deployement
     ],
   }),
 );
